2eadd84affa12d3c6fa30f8e5bd3f321f58193c7
Stabilo <zteo015@e.ntu.edu.sg>
2022-01-16 15:22:15 +0800

    Added simple chatty bot and rearrangled files according to stages

---
+++ Stage1/ChattyBot.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// ZY, 16 Jan 2022, 3.19pm
+// Learning Go through JetBrains Academy free lessons
+//Intro to simple chatty bot
+
+func main() {
+	fmt.Println("Hello! My name is robot.")
+	fmt.Println("I was created in 2022.")
+}
